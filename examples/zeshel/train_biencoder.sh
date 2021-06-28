c5c3734dc48f5ca46035ef679c84a0c9f90f63aa
Rungsiman Nararatwong <rungsiman@gmail.com>
2021-06-28 17:39:53 +0000

    Add example scripts for bi-encoder

---
+++ examples/zeshel/train_biencoder.sh
@@ -0,0 +1,7 @@
+python blink/biencoder/train_biencoder.py \
+  --top_k 64 \
+  --learning_rate 2e-5 \
+  --train_batch_size 128 \
+  --num_train_epochs 5 \
+  --data_path data/zeshel/blink_format \
+  --output_path /root/BLINK/output/zeshel/models
